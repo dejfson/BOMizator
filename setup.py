f7920bb49508ad7961cda8d119b9fce59e483b37
David Belohrad <david@belohrad.ch>
2017-02-23 17:09:24 +0100

    python application skeleton imported

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='farnellBOM',
+      version = '0.1',
+      packages = ['farnellBOM',],
+      description = "KiCad BOM CSV to Farnell linker",
+      author = 'David Belohrad',
+      author_email = 'david.belohrad@cern.ch',
+      install_requires = ['csv'],
+      scripts=['bin/farnellBOM', ]
+      )
