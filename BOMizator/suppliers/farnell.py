5baa99fbb7db6bcd3879c3802282aae4c56cd35d
David Belohrad <david@belohrad.ch>
2017-03-01 00:07:40 +0100

    repaired bug/typo in farnell supplier filter

--- BOMizator/suppliers/farnell.py
+++ BOMizator/suppliers/farnell.py
@@ -119,7 +119,7 @@ wcs/stores/servlet/Search?st=%s" % (searchtext,)
             data = (manufacturer, reference,
                     QtCore.QString("FARNELL"), partnum)
             # datasheet must stay as is as web pages might be case sensitive
-            dataupper = list(map(QtCore.QString.toUpper,am
+            dataupper = list(map(QtCore.QString.toUpper,
                                  data)) + [datasheet, ]
             return dict(zip(datanames, dataupper))
 
