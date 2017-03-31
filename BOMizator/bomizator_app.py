2fc568d461324d599605f36a2c90011c30638e41
David Belohrad <david@belohrad.ch>
2017-03-31 16:07:17 +0200

    webbrowser now dynamically selected

--- BOMizator/bomizator_app.py
+++ BOMizator/bomizator_app.py
@@ -1184,8 +1184,19 @@ class BOMizator(QtWidgets.QMainWindow, form_class):
         """ opens the browser with datasheet
         """
         # now fire the web browser with this page opened
-        b = webbrowser.get('firefox')
-        b.open(page, new=0, autoraise=True)
+        # this is list of browsers in the list of
+        # 'preferences'. Konqueror has troubles to display octopart
+        browsers = ['firefox', 'google-chrome', 'windows-default']
+        while True:
+            brw = browsers.pop(0)
+            if not brw:
+                raise webbrowser.Error("Cound not locate runnable browser")
+            try:
+                b = webbrowser.get(brw)
+                b.open(page, new=0, autoraise=True)
+                return
+            except webbrowser.Error:
+                pass
 
     def openSearchBrowser(self, searchtext):
         """ This function calls default plugin to supply the web
@@ -1214,20 +1225,7 @@ class BOMizator(QtWidgets.QMainWindow, form_class):
                                   su.replace(" ", "%20"), ),
                               self.model.suppliers.plugins.keys()))
         url = "https://octopart.com/search?q=%s&" % (searchtext)+limits
-        # now fire the web browser with this page opened
-        # this is list of browsers in the list of
-        # 'preferences'. Konqueror has troubles to display octopart
-        browsers = ['firefox', 'google-chrome', 'windows-default']
-        while True:
-            brw = browsers.pop(0)
-            if not brw:
-                raise webbrowser.Error("Cound not locate runnable browser")
-            try:
-                b = webbrowser.get(brw)
-                b.open(url, new=0, autoraise=True)
-                return
-            except webbrowser.Error:
-                pass
+        self.openBrowser(url)
 
     def treeDoubleclick(self, index):
         """ when user doubleclicks item, we search for it in farnel
